f80d3288e95ff90735b38b52f38c3a75c870e372
우재현 <jaehyun.woo@minigate.net>
2024-03-28 17:42:28 +0900

    Player Default Attakc Animation

---
+++ IdleGame/Assets/Animations/Player/AttackReset.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class AttackReset : StateMachineBehaviour
+{
+    [SerializeField] string triggerName;
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
+    {
+        animator.ResetTrigger(triggerName);
+    }
+}
