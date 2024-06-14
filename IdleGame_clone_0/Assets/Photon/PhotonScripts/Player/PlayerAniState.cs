7f3bf2ea6ed191fe79c7f883b6f9af8bba33e63c
우재현 <jaehyun.woo@minigate.net>
2024-06-14 17:22:02 +0900

    클론 에디터 제거

--- IdleGame_clone_0/Assets/Photon/PhotonScripts/Player/PlayerAniState.cs
+++
@@ -1,7 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-namespace Photon_NetWork
-{
-    public enum PlayerAniState { None, Jump, AttackLeft, AttackRight, GunRight, Hit, Death }
-}
